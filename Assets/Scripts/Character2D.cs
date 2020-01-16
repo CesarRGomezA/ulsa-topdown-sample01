29fc03949dbf8cc384be09149334d677daabf14c
CesarRGomezA <CesarRGomezA@outlook.com>
2020-01-16 08:54:46 -0600

    coliciona

--- Assets/Scripts/Character2D.cs
+++ Assets/Scripts/Character2D.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class Character2D : MonoBehaviour
 {
+    [SerializeField]
+    //si deseas que se vea
+    float moveSpeed = 5f;
+    //si no deseas que se vea
+    //float moveSpeed {get; set;}
+    
     void Update()
     {
-        transform.Translate(Vector2.right * 2f * Time.deltaTime);
+        transform.Translate(Axis * 2f * Time.deltaTime);
     }
+
+    Vector2 Axis
+    {
+        //metodo get (palabra reservada)
+         get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+    public float MoveSpeed { get => moveSpeed; set => moveSpeed = value; }
 }
